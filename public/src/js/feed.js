02ccf46ad62774082443e3b0757c85ee53c30c46
chiangs <stephen.e.chiang@gmail.com>
2018-04-15 22:16:43 +0200

    Example http requests, and polyfills added

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -1,13 +1,27 @@
 var shareImageButton = document.querySelector('#share-image-button');
 var createPostArea = document.querySelector('#create-post');
-var closeCreatePostModalButton = document.querySelector('#close-create-post-modal-btn');
+var closeCreatePostModalButton = document.querySelector(
+	'#close-create-post-modal-btn'
+);
 
+// Function called when user taps on add button, a place to potentially show `Add to Homescreen`
 function openCreatePostModal() {
-  createPostArea.style.display = 'block';
+	createPostArea.style.display = 'block';
+	if (deferredPrompt) {
+		deferredPrompt.prompt();
+		deferredPrompt.userChoice.then(choice => {
+			console.log(choice.outcome);
+			choice.outcome === 'dismissed'
+				? console.log('User denied Add2H')
+				: console.log('Added to Homescreen!');
+		});
+		// Set to null as we only get one chance
+		deferredPrompt = null;
+	}
 }
 
 function closeCreatePostModal() {
-  createPostArea.style.display = 'none';
+	createPostArea.style.display = 'none';
 }
 
 shareImageButton.addEventListener('click', openCreatePostModal);
--- public/sw.js
+++ public/sw.js
@@ -2,21 +2,22 @@
 // self.addEventListener('install', function(event) {
 // 	console.log('[Service Worker] Installing service worker ...', event);
 // });
-self.addEventListener(`install`, event =>
+self.addEventListener('install', event =>
 	console.log('[Service Worker] Installing service worker ...', event)
 );
 
 // when sw installed AND activated
 // all running tabs of this web app must be closed in order for this to register
 // this is to not interrupt and break current communication with old version of SW
-self.addEventListener(`activate`, event => {
-	console.log(`[Service Worker] Activating service worker ...`, event);
+self.addEventListener('activate', event => {
+	console.log('[Service Worker] Activating service worker ...', event);
 	return self.clients.claim();
 });
 
 // fetch happens when something is loaded like image/css/scripts when we send manual fetch request
-self.addEventListener(`fetch`, event => {
-	console.log(`[Service Worker] Fetching something ...`, event);
+// can listen for all fetch methods
+self.addEventListener('fetch', event => {
+	console.log('[Service Worker] Fetching something ...', event);
 	event.respondWith(fetch(event.request));
 	// currently only send the request data, but this is where you can decide what to do based on connection status
 });
